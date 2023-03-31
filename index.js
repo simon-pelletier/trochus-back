ce49228433e465fc114bc9513289769d3ab38aab
simon-pelletier <symasp@gmail.com>
2023-03-31 08:40:12 +0200

    feat(global): add basic cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3001;
 
-const router = require('./app/router.js')
+const router = require('./app/router.js');
+
+app.use(cors());
 
 app.use(router);
 
